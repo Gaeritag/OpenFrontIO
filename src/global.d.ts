f9610cb45bf623aa3870e8d779f1e88fee3bce7b
icslucas <carolinacarazolli@gmail.com>
2025-03-30 07:20:35 -0300

    Update global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,4 @@
+import 'express-session';
 declare module "*.png" {
   const content: string;
   export default content;
@@ -35,4 +36,15 @@ declare module "*.html" {
 declare module "*.json" {
   const value: any;
   export default value;
+}
+declare module 'express-session' {
+  interface SessionData {
+    user?: {
+      id: string;
+      username: string;
+      discriminator: string;
+      avatar: string;
+      accessToken: string;
+    };
+  }
 }
